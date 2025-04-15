8ec035333550511420135727b12aab7ea8c4c480
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-15 15:45:02 +0200

    Implement show route to retrieve movie details and associated reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -19,7 +19,33 @@ function index(req, res) {
 
 //show route
 function show(req, res) {
-  res.send(`Show route for id: ${req.params.id}`);
+
+  //get id from params
+  const id = Number(req.params.id);
+
+  //sql queries
+  const sql = `SELECT * FROM movies WHERE movies.id = ?`;
+  const sqlReviews = `SELECT * FROM reviews WHERE reviews.movie_id = ?`;
+
+  //perform sql query
+  connection.query(sql, [id], (err, results) => {
+    //handle errors
+    if (err) return res.status(500).json({ error: err.message });
+    if (results.length === 0) return res.status(404).json({ message: "No movies founded!" });
+
+    //get the result and save it in a variable
+    const movie = results[0];
+
+    //perform second query to get the movie reviews
+    connection.query(sqlReviews, [id], (err, results) => {
+      if (err) return res.status(500).json({ error: err.message });
+
+      //return the results
+      movie.reviews = results;
+      res.json(movie);
+    });
+  });
+
 }
 
 //exports module
